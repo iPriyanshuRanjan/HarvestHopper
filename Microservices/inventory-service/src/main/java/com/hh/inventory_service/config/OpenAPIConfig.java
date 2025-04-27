275adeaf65f10e7f77cad7f73e6b38f2a521415b
Priyanshu Ranjan <Prynshurnjn@gmail.com>
2025-04-27 22:47:52 +0530

    added swagger in all the services

---
+++ Microservices/inventory-service/src/main/java/com/hh/inventory_service/config/OpenAPIConfig.java
@@ -0,0 +1,23 @@
+package com.hh.inventory_service.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI InventoryServiceAPI(){
+        return new OpenAPI().info(new Info().title("Inventory Service API")
+                        .description("This is the rest API documentation for the Inventory Service")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("You can refer to the Inventory Service Wiki Document")
+                        .url("xyz.com"));
+    }
+}
+
